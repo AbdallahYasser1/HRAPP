57ae050d0b0db3ca69d62dd8d2f2f42cf4f1743c
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2022-05-16 23:23:21 +0200

    update img

--- app/Http/Controllers/MissionUpdatesController.php
+++ app/Http/Controllers/MissionUpdatesController.php
@@ -23,7 +23,7 @@ class MissionUpdatesController extends ApiController
             return $this->errorResponse("MissionUpdate in pending status or time of update vanished",400);
         }
         if($request->hasFile('approved_file')){
-            $path=$request->file('approved_file')->store('public/missionUpdateImages');
+            $path=cloudinary()->upload($request->file('approved_file')->getRealPath(),$options = ['folder'=>'missionUpdateImages'])->getSecurePath();
             $missionUpdate=new MissionUpdate(['description'=>$request['description'],'date'=>$request['date'],'extra_cost'=>$request['extra_cost'],'approved_file'=>$path]);
             $mission->missionUpdates()->save($missionUpdate);
             return $this->showCustom("mission update saved", 200);
