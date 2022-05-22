fa79fae2dfad0f16c27873a2031df20913824815
HassanRady <sim.hassan.khaled@alexu.edu.eg>
2022-05-22 17:58:24 +0200

    config update

--- database/migrations/2022_04_24_014954_create_configs_table.php
+++ database/migrations/2022_04_24_014954_create_configs_table.php
@@ -16,16 +16,20 @@ return new class extends Migration
         Schema::create('configs', function (Blueprint $table) {
             $table->id();
             $table->string('company_name');
-            $table->string('specifity');
+            $table->string('specificity');
             $table->string('company_email');
             $table->string('company_phone');
             $table->string('branches');
             $table->string('location');
             $table->string('country');
             $table->string('photo');
-            $table->string('latiude');
-            $table->string('longtiude');
+            $table->string('latitude');
+            $table->string('longitude');
             $table->string('distance');
+            $table->string('timezone');
+            $table->string('fullDayAbsenceDeduction');
+            $table->string('halfDayAbsenceDeduction');
+            
             $table->timestamps();
         });
     }
--- database/seeders/ConfigSeeder.php
+++ database/seeders/ConfigSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+
 class ConfigSeeder extends Seeder
 {
     /**
@@ -15,20 +16,18 @@ class ConfigSeeder extends Seeder
      */
     public function run()
     {
-         $configration = [
-            'company_name'=> 'EvaPay'
-            , 'specifity'=> 'Software'
-            , 'company_email'=> 'eva@pay.com'
-            , 'company_phone'=> '0121212'
-           ,'country'=> 'egypt' ,
+        $configuration = [
+            'company_name' => 'EvaPay', 'specificity' => 'Software', 'company_email' => 'eva@pay.com', 'company_phone' => '0121212', 'country' => 'egypt',
             'branches' => '1',
- 'location' => 'alex',
- 'longtiude' => 'long',
- 'latiude' => 'lat',
- 'distance' => 'lat',
- 'photo' => 'lat',
+            'location' => 'alex',
+            'longitude' => '29.955596923828125',
+            'latitude' => '31.213575888245597',
+            'distance' => '2000',
+            'photo' => 'lat',
+            'timezone' => 'Africa/Cairo',
+            'fullDayAbsenceDeduction' => '1/30',
+            'halfDayAbsenceDeduction' => '1/60',
         ];
-        DB::table('configs')->insert($configration);
-
+        DB::table('configs')->insert($configuration);
     }
 }
