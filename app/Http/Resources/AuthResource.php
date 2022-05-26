9373ccaf1be5d9ca66283d3fbdd1bf47e6a4aaac
AbdallahYasser1 <a.yasser9999@yahoo.com>
2022-05-26 21:27:35 +0200

    login show job title

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -18,6 +18,7 @@ class AuthResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'job_title' => $this->profile->job_title->job_name,
             'phone' => $this->phone,
             'birthdate'=>$this->birthdate,
             'first_time_login'=>$this->first_time_login==1?true:false,
