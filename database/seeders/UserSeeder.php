995fe9eca55e1b332f9049694891cbd5a2ce1fd4
AbdallahYasser1 <a.yasser9999@yahoo.com>
2022-06-01 23:34:02 +0200

    photo

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,16 +21,16 @@ class UserSeeder extends Seeder
     public function run()
     {
         //User::factory(10)->create();
-        User::factory(1)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => 1,'job__title_id'=>1]))->create()
+        User::factory(1)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => 1,'job__title_id'=>1, "image"=>"https://res.cloudinary.com/dokaaek9w/image/upload/v1653746912/profile_images/IMG-20220403-WA0021_yvig6b.jpg"]))->create()
             ->each(function ($user) {
                 $user->assignRole('Admin');
             });
 
-        User::factory(1)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => 2,'job__title_id'=>2]))->create()
+        User::factory(1)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => 2,'job__title_id'=>2, "image"=>"https://res.cloudinary.com/dokaaek9w/image/upload/v1653746912/profile_images/IMG-20220403-WA0021_yvig6b.jpg"]))->create()
             ->each(function ($user) {
                 $user->assignRole('HR');
             });
-        User::factory(3)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => 3,'job__title_id'=>3]))->create()
+        User::factory(3)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => 3,'job__title_id'=>3, "image"=>"https://res.cloudinary.com/dokaaek9w/image/upload/v1653746912/profile_images/IMG-20220403-WA0021_yvig6b.jpg"]))->create()
             ->each(function ($user) {
                 $user->assignRole('Accountant');
             });
@@ -39,16 +39,16 @@ class UserSeeder extends Seeder
             ->each(function ($user) {
                 $user->assignRole('Normal');
             });
-        User::factory(10)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => $department->id,'job__title_id'=>$department->id]))->state(['supervisor' => $normalUser[0]->id])->create()
+        User::factory(10)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => $department->id,'job__title_id'=>$department->id , "image"=>"https://res.cloudinary.com/dokaaek9w/image/upload/v1653746912/profile_images/IMG-20220403-WA0021_yvig6b.jpg"] ))->state(['supervisor' => $normalUser[0]->id])->create()
             ->each(function ($user) {
                 $user->assignRole('Normal');
             });
         $department = Department::all()->random();
-        $normalUser2 = User::factory(1)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => $department->id,'job__title_id'=>$department->id]))->create()
+        $normalUser2 = User::factory(1)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => $department->id,'job__title_id'=>$department->id , "image"=>"https://res.cloudinary.com/dokaaek9w/image/upload/v1653746912/profile_images/IMG-20220403-WA0021_yvig6b.jpg"]))->create()
             ->each(function ($user) {
                 $user->assignRole('Normal');
             });
-        User::factory(10)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => $department->id,'job__title_id'=>$department->id]))->state(['supervisor' => $normalUser2[0]->id])->create()
+        User::factory(10)->has(Vacationday::factory()->state([]))->has(Profile::factory()->state(['department_id' => $department->id,'job__title_id'=>$department->id , "image"=>"https://res.cloudinary.com/dokaaek9w/image/upload/v1653746912/profile_images/IMG-20220403-WA0021_yvig6b.jpg"] ))->state(['supervisor' => $normalUser2[0]->id])->create()
             ->each(function ($user) {
                 $user->assignRole('Normal');
             });
