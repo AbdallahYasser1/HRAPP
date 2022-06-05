fe56d90a88e64b90e488980fb505fdaea91c805f
AbdallahYasser1 <a.yasser9999@yahoo.com>
2022-06-05 22:58:15 +0200

    tets

--- database/migrations/2022_05_15_155746_create_absences_table.php
+++ database/migrations/2022_05_15_155746_create_absences_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('absences', function (Blueprint $table) {
-            $table->id('id');
+            $table->id();
             $table->date("date");
             $table->integer('user_id');
             $table->enum('status',['wfh','vacation','leave','absent'])->default('absent');
