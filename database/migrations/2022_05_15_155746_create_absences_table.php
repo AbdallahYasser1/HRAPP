58b0b42738dea68487808489839c2c001b30a3d8
AbdallahYasser1 <a.yasser9999@yahoo.com>
2022-06-05 22:33:21 +0200

    hi

--- database/migrations/2022_05_15_155746_create_absences_table.php
+++ database/migrations/2022_05_15_155746_create_absences_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->enum('status',['wfh','vacation','leave','absent'])->default('absent');
+            $table->primary(['user_id', 'date']);
             $table->timestamps();
         });
     }
