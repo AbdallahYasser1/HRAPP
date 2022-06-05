6d842ad126fc4d9e8e47f6c8fb669154877c504a
AbdallahYasser1 <a.yasser9999@yahoo.com>
2022-06-05 22:36:07 +0200

    push

--- database/migrations/2022_05_15_155746_create_absences_table.php
+++ database/migrations/2022_05_15_155746_create_absences_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('absences', function (Blueprint $table) {
-            $table->id();
+         
             $table->date("date");
             $table->integer('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
