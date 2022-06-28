048835b8fc638020257c529e261e273c3a2ca1dc
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2022-06-28 15:43:15 +0200

    Update MissionUpdatesController.php

--- app/Http/Controllers/MissionUpdatesController.php
+++ app/Http/Controllers/MissionUpdatesController.php
@@ -17,7 +17,7 @@ $request['date']=date('Y-m-d');
         if($mission===null){
             return $this->errorResponse("Mission not found",404);
         }
-        if(!in_array($mission->requests()->first()->status, ['approved','closed','in-progress'])||(strtotime($mission->requests()->first()->end_date)<strtotime($request['date'])||strtotime($mission->requests()->first()->start_date)>strtotime($request['date']))||$mission->requests()->first()->user_id!=Auth::id()){
+        if(!in_array($mission->requests()->first()->status, ['approved','closed','in-progress'])||(strtotime($mission->requests()->first()->end_date)<strtotime($request['date'])||strtotime($mission->requests()->first()->start_date)>strtotime($request['date']))||$mission->requests()->first()->user_id!=Auth::id()||$mission->paid){
           // print_r($mission->requests()->first()->status!='approved');
 
             return $this->errorResponse("MissionUpdate in pending status or time of update vanished",400);
