458270d14486cb40ed23e9d891793f04aba8206d
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2022-06-28 15:37:58 +0200

    Update CalulateTotalMission.php

--- app/Traits/CalulateTotalMission.php
+++ app/Traits/CalulateTotalMission.php
@@ -6,7 +6,10 @@ trait CalulateTotalMission
     public function calculateTotalMissionAndMissionUpdates(Mission $mission){
         $combineResult=[];
         $combineResult['mission_id']=$mission->id;
+        $combineResult['mission_description']=$mission->description;
+        $combineResult['mission_paid']=$mission->paid;
         $combineResult['user_id']=$mission->requests->first()->user_id;
+        $combineResult['request_id']=$mission->requests->first()->id;
         $combineResult['initial_cost']=$mission->initial_cost;
         $combineResult['extra_cost']=$mission->missionUpdates->sum('extra_cost');
         $combineResult['total']=$combineResult['extra_cost']+$combineResult['initial_cost'];
